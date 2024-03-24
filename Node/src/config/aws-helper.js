c314b07426a28a80e001bac8be07ebcd20ec2616
UyenQuyen <uynwiin@gmail.com>
2024-03-24 22:51:02 +0700

    Test push without .env

--- Node/src/config/aws-helper.js
+++ Node/src/config/aws-helper.js
@@ -6,7 +6,7 @@ process.env.AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE = "1";
 AWS.config.update({
     region: process.env.REGION,
     accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.ACCESS_KEY_SECRET
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
 })
 
 const s3 = new AWS.S3();
